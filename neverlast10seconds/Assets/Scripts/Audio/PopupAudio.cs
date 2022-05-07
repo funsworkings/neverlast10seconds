05245bfcd52dd03117d3a174048faa2c71342c41
kghobadi <kasghobadi@gmail.com>
2022-05-07 19:43:31 -0400

    all the audio stuff

---
+++ neverlast10seconds/Assets/Scripts/Audio/PopupAudio.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupAudio : AudioHandler
+{
+    public AudioClip[] popupOpenSounds;
+    public AudioClip[] popupCloseSounds;
+    public AudioClip[] popupClickSounds;
+}
