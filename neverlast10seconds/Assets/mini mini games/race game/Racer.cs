1870e8b6c60b71901eca0640fc9c23118c2bc7a2
julian pc laptop <juliancorderod@gmail.com>
2022-05-07 21:42:38 -0400

    pucca movement feeling sharp

--- neverlast10seconds/Assets/mini mini games/race game/Racer.cs
+++ neverlast10seconds/Assets/mini mini games/race game/Racer.cs
@@ -7,7 +7,7 @@ public class Racer : MonoBehaviour
     Rigidbody r;
     public float maxSpeed = 10;
     public float maxAccel = 10;
-    public float maxTurn = 10;
+    public float maxTurnSpeed = 10;
     public float gravity = 10;
 
     void Start()
@@ -24,21 +24,60 @@ public class Racer : MonoBehaviour
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
     }
 
+    Quaternion groundAlignment = Quaternion.identity;
     void FixedUpdate()
     {
+        if (onGround) groundNormal.Normalize();
+        else groundNormal = Vector3.up;
+
+        AlignToGround();
+
+        DoVel();
+
+        onGround = false;
+        groundNormal = Vector3.zero;
+    }
+
+    void AlignToGround()
+    {
+        r.rotation = Quaternion.Lerp(r.rotation,
+                        Quaternion.FromToRotation(r.rotation * Vector3.up, groundNormal) * r.rotation,
+                        onGround ? 0.2f : 0.01f);
+        //dont love this lerp
+    }
+
+    void DoVel()
+    {
         vel = r.velocity;
-        Vector3 fwd = transform.TransformDirection(transform.forward);
-        vel = Vector3.MoveTowards(vel, transform.forward * playerInput.y * maxSpeed, maxAccel);
+        angVel = r.angularVelocity;
+
+        Vector3 xAxis = ProjectOnGroundPlane(Vector3.right).normalized;
+        Vector3 zAxis = ProjectOnGroundPlane(Vector3.forward).normalized;
+
+        float currentX = Vector3.Dot(vel, xAxis);
+        float currentZ = Vector3.Dot(vel, zAxis);
+
+        Vector3 targVel = Vector3.ProjectOnPlane(transform.forward, Vector3.up) * playerInput.y * maxSpeed;
+
+        float newX = Mathf.MoveTowards(currentX, targVel.x, onGround ? maxAccel : maxAccel * .1f);
+        float newZ = Mathf.MoveTowards(currentZ, targVel.z, onGround ? maxAccel : maxAccel * .1f);
+
+        vel += xAxis * (newX - currentX) + zAxis * (newZ - currentZ);
+
         if (!onGround) vel.y -= gravity;
         r.velocity = vel;
 
-        angVel = r.angularVelocity;
-        angVel.y = Mathf.MoveTowards(angVel.y, playerInput.x * maxTurn, maxAccel);
+        float maxTurnAllowed = Mathf.Abs(playerInput.y);
+        float turnDir = Mathf.Sign(playerInput.y);
+        angVel.y = Mathf.MoveTowards(angVel.y,
+                                     playerInput.x * maxTurnSpeed * maxTurnAllowed * turnDir,
+                                     onGround ? maxAccel : maxAccel * .1f);
         r.angularVelocity = angVel;
-
-        onGround = false;
     }
+
+
     public bool onGround;
+    Vector3 groundNormal;
     void OnCollisionEnter(Collision collision) { EvaluateCollision(collision); }
     void OnCollisionStay(Collision collision) { EvaluateCollision(collision); }
     void EvaluateCollision(Collision collision)
@@ -46,8 +85,17 @@ public class Racer : MonoBehaviour
         for (int i = 0; i < collision.contactCount; i++)
         {
             Vector3 normal = collision.GetContact(i).normal;
-            onGround |= normal.y >= 0.9f;
+            if (normal.y >= 0.5f)//this should change depending on speed!
+            {
+                onGround = true;
+                groundNormal += normal;
+            }
         }
+    }
+
+    Vector3 ProjectOnGroundPlane(Vector3 vector)
+    {
+        return vector - groundNormal * Vector3.Dot(vector, groundNormal);
     }
 }
 
