0e10be7bcf8cc6349b332862a025ab11da9675bc
julian pc laptop <juliancorderod@gmail.com>
2022-05-08 17:50:31 -0400

    okie minigame close i guess sorry this is so bad

--- neverlast10seconds/Assets/mini mini games/race game/Racer.cs
+++ neverlast10seconds/Assets/mini mini games/race game/Racer.cs
@@ -20,21 +20,24 @@ public class Racer : MonoBehaviour
 
     void Update()
     {
-        playerInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        DoTapSpeed();
     }
 
     Quaternion groundAlignment = Quaternion.identity;
     void FixedUpdate()
     {
-        if (onGround) groundNormal.Normalize();
-        else groundNormal = Vector3.up;
+        if (OnGround)
+        {
+            if (groundContactCount > 1)
+                groundNormal.Normalize();
+        }
+        else groundNormal = transform.up;
 
         AlignToGround();
 
         DoVel();
 
-        onGround = false;
+        groundContactCount = 0;
         groundNormal = Vector3.zero;
     }
 
@@ -42,10 +45,11 @@ public class Racer : MonoBehaviour
     {
         r.rotation = Quaternion.Lerp(r.rotation,
                         Quaternion.FromToRotation(r.rotation * Vector3.up, groundNormal) * r.rotation,
-                        onGround ? 0.2f : 0.01f);
+                        OnGround ? .3f : 0.05f);
         //dont love this lerp
+        //also gotta do something about the jitter
     }
-
+    float speed;
     void DoVel()
     {
         vel = r.velocity;
@@ -57,26 +61,61 @@ public class Racer : MonoBehaviour
         float currentX = Vector3.Dot(vel, xAxis);
         float currentZ = Vector3.Dot(vel, zAxis);
 
-        Vector3 targVel = Vector3.ProjectOnPlane(transform.forward, Vector3.up) * playerInput.y * maxSpeed;
 
-        float newX = Mathf.MoveTowards(currentX, targVel.x, onGround ? maxAccel : maxAccel * .1f);
-        float newZ = Mathf.MoveTowards(currentZ, targVel.z, onGround ? maxAccel : maxAccel * .1f);
+        float angVelChange = Mathf.Abs(angVel.y) - Mathf.Abs(prevAngVel.y);
+        if (angVelChange > 0.1f)
+            speed *= Remaps.EaseInQuad(angVelChange, 0, 8, 1, 0);
 
-        vel += xAxis * (newX - currentX) + zAxis * (newZ - currentZ);
+        float targSpeed = maxSpeed * fwdTapSpeed;// Mathf.Max(playerInput.y, -0.15f);
+        speed = Mathf.MoveTowards(speed, targSpeed, OnGround ? maxAccel : maxAccel * .1f);
+        Vector3 targVel = Vector3.ProjectOnPlane(transform.forward, Vector3.up) * speed;
+        vel += xAxis * (targVel.x - currentX) + zAxis * (targVel.z - currentZ);
 
-        if (!onGround) vel.y -= gravity;
+        // Vector3 targVel = Vector3.ProjectOnPlane(transform.forward, groundNormal) * playerInput.y * maxSpeed;
+
+        // float newX = Mathf.MoveTowards(currentX, targVel.x, OnGround ? maxAccel : maxAccel * .1f);
+        // float newZ = Mathf.MoveTowards(currentZ, targVel.z, OnGround ? maxAccel : maxAccel * .1f);
+
+        // vel += xAxis * (newX - currentX) + zAxis * (newZ - currentZ);
+
+
+
+        if (!OnGround) vel.y -= gravity;
         r.velocity = vel;
 
-        float maxTurnAllowed = Mathf.Abs(playerInput.y);
-        float turnDir = Mathf.Sign(playerInput.y);
+        float maxTurnAllowed = 1;// Mathf.Abs(fwdTapSpeed);
+        float turnDir = Mathf.Sign(fwdTapSpeed);
         angVel.y = Mathf.MoveTowards(angVel.y,
-                                     playerInput.x * maxTurnSpeed * maxTurnAllowed * turnDir,
-                                     onGround ? maxAccel : maxAccel * .1f);
-        r.angularVelocity = angVel;
+                                    (rightTapSpeed - leftTapSpeed) * maxTurnSpeed * maxTurnAllowed * turnDir,
+                                     OnGround ? maxAccel : maxAccel * .1f);
+        r.angularVelocity = prevAngVel = angVel;
+
+    }
+    float fwdTapSpeed, leftTapSpeed, rightTapSpeed;
+    public float tapSpeedIncrement, tapSpeedSlowDown;
+    void DoTapSpeed()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) fwdTapSpeed += tapSpeedIncrement;
+        fwdTapSpeed -= Time.deltaTime * tapSpeedSlowDown;
+        fwdTapSpeed = Mathf.Max(fwdTapSpeed, 0);
+
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) leftTapSpeed += tapSpeedIncrement;
+        leftTapSpeed -= Time.deltaTime * tapSpeedSlowDown;
+        leftTapSpeed = Mathf.Max(leftTapSpeed, 0);
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) rightTapSpeed += tapSpeedIncrement;
+        rightTapSpeed -= Time.deltaTime * tapSpeedSlowDown;
+        rightTapSpeed = Mathf.Max(rightTapSpeed, 0);
+
+        // print("fwd: " + fwdTapSpeed + ", r: " + rightTapSpeed + ", l: " + leftTapSpeed);
     }
 
+    Vector3 prevAngVel;
 
-    public bool onGround;
+
+    int groundContactCount;
+    bool OnGround => groundContactCount > 0;
     Vector3 groundNormal;
     void OnCollisionEnter(Collision collision) { EvaluateCollision(collision); }
     void OnCollisionStay(Collision collision) { EvaluateCollision(collision); }
@@ -87,7 +126,7 @@ public class Racer : MonoBehaviour
             Vector3 normal = collision.GetContact(i).normal;
             if (normal.y >= 0.5f)//this should change depending on speed!
             {
-                onGround = true;
+                groundContactCount += 1;
                 groundNormal += normal;
             }
         }
