213b62875c5aef319a3d4c4d2b97ab9feda78329
kghobadi <kasghobadi@gmail.com>
2022-05-07 22:22:08 -0400

    trying to get the minigame scene loading -> render texture set up working with additive scene loading.
    - still broked :_ )

--- neverlast10seconds/Assets/Scripts/cumOS/Overworld/BrowserController.cs
+++ neverlast10seconds/Assets/Scripts/cumOS/Overworld/BrowserController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using cumOS.UIShit;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 namespace cumOS.Overworld
 {
@@ -16,6 +18,13 @@ namespace cumOS.Overworld
         [SerializeField] private BrowserUITab tabPrefab;
         [SerializeField] private BrowserWindow browserWindowPrefab;
 
+        [Header("Mini-game loading.")]
+        [Tooltip("When the random roll is less than this value, it is a mini-game browser window.")]
+        public float minigameChance;
+        [Tooltip("Array of possible mini-game scenes to load.")]
+        public string[] possibleGameScenesToLoad;
+        public List< RenderTexture>  unusedRenderTextures = new List<RenderTexture>();
+        public List< RenderTexture> usedRendertextures = new List<RenderTexture>();
         private void Awake()
         {
             _window = GetComponent<UIWindow>();
@@ -35,16 +44,84 @@ namespace cumOS.Overworld
         public void AddBrowserWindow(BrowserWindow browserWindow)
         {
             AddWindow(browserWindow);
+            MinigameCheck(browserWindow);
         }
 
         public BrowserUITab RequestTab(BrowserWindow window)
         {
             var tab = Instantiate(tabPrefab, tabsRoot);
+
             tab.Initialize(window, window.thumbnail);
             
             tab.transform.SetSiblingIndex(window.transform.GetSiblingIndex()); // Match window
 
             return tab;
+        }
+
+        /// <summary>
+        /// Checks if this window should be a mini-game. 
+        /// </summary>
+        public void MinigameCheck(BrowserWindow browserWindow)
+        {
+            float randomChance = Random.Range(0f, 100f);
+
+            if (randomChance < minigameChance)
+            {
+                LoadRandomMinigame(browserWindow);
+            }
+        }
+
+        /// <summary>
+        /// Loads a random mini-game scene additively. 
+        /// </summary>
+        public void LoadRandomMinigame(BrowserWindow browserWindow)
+        {
+            string sceneName = possibleGameScenesToLoad[Random.Range(0, possibleGameScenesToLoad.Length)];
+            Scene minigame = SceneManager.GetSceneByName( sceneName);
+            SceneManager.LoadScene(minigame.name, LoadSceneMode.Additive);
+            browserWindow.SetMinigame(minigame);
+        }
+        
+        //TODO will need an unload scene function when we close a window. 
+        //https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager.UnloadSceneAsync.html
+
+        /// <summary>
+        /// Assign the first unused render texture to the minigame in the browser window. 
+        /// </summary>
+        /// <param name="window"></param>
+        public void AssignRenderTextureToMinigame(Scene scene, BrowserWindow window)
+        {
+            //get the render texture
+            RenderTexture renderToUse = unusedRenderTextures[0];
+            
+            //get all objects in the scene we loaded
+            GameObject[] minigameObjects = scene.GetRootGameObjects();
+            //loop thru them
+            foreach (var sceneObj in  minigameObjects)
+            {
+                //looking for the main cam
+                if (sceneObj.CompareTag("MainCamera"))
+                {
+                    //get the camera component
+                    Camera sceneCam = sceneObj.GetComponent<Camera>();
+                    //assign the target render texture.
+                    sceneCam.targetTexture = renderToUse;
+                    break;
+                }
+            }
+            
+            //assign the render texture to our raw image in the window. 
+            window.AssignRenderTexture(renderToUse);
+            //remove from circulation. 
+            usedRendertextures.Add(renderToUse);
+            unusedRenderTextures.RemoveAt(0);
+        }
+
+        //Removes render texture from used and adds back to unused. 
+        public void DeactivateRenderTexture(RenderTexture renderTexture)
+        {
+            usedRendertextures.Remove(renderTexture);
+            unusedRenderTextures.Add(renderTexture);
         }
     }
 }
--- neverlast10seconds/Assets/Scripts/cumOS/Overworld/BrowserWindow.cs
+++ neverlast10seconds/Assets/Scripts/cumOS/Overworld/BrowserWindow.cs
@@ -1,13 +1,14 @@
 using System;
 using cumOS.UIShit;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace cumOS.Overworld
 {
     public class BrowserWindow : UIWindow
     {
         // Properties
-
         BrowserUITab _tab;
         BrowserUITab tab
         {
@@ -23,6 +24,20 @@ namespace cumOS.Overworld
         }
         
         public Sprite thumbnail;
+        
+        [Header("Minigame Settings")]
+        public bool isMinigame;
+        public Scene loadedMinigame;
+        private RawImage minigameImage;
+        private RenderTexture renderTexture;
+
+        public RawImage MinigameImage
+        {
+            get
+            {
+                return minigameImage;
+            }
+        }
 
         protected override void Start()
         {
@@ -30,6 +45,25 @@ namespace cumOS.Overworld
             
             draggable = false;
             tab.SetColor(color);
+        }
+
+        public void SetMinigame(Scene loadedGame)
+        {
+            isMinigame = true;
+            loadedMinigame = loadedGame;
+            //destroy image
+            Image imageComp = GetComponent<Image>();
+            Destroy(imageComp);
+            //add raw image instead
+            minigameImage = gameObject.AddComponent<RawImage>();
+            //assign the render texture :)
+            (manager as BrowserController).AssignRenderTextureToMinigame(loadedGame, this);
+        }
+
+        public void AssignRenderTexture(RenderTexture texture)
+        {
+            renderTexture = texture;
+            MinigameImage.texture = texture;
         }
 
         public override void Show()
@@ -45,6 +79,12 @@ namespace cumOS.Overworld
             
             if (_tab != null)
             {
+                if (MinigameImage != null)
+                {
+                    //tell BrowserController take my render texture out of circulation. 
+                    (manager as BrowserController).DeactivateRenderTexture(renderTexture);
+                }
+                
                 _tab.gameObject.SetActive(false);
             }
         }
